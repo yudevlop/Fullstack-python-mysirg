ecfd1fed0bfd768d736f58d581272b3ee15ffb38
yudevlop <helpyakeen@gmail.com>
2023-06-05 15:42:45 +0530

    Create assign1.py

---
+++ assign1.py
@@ -0,0 +1,15 @@
+# Assignment 1 Solution By Ayush Gupta 
+""" Que 1  
+Ans - Guido Van Rossum 
+Que 2 
+Ans  first version of python is developed in 1994
+Que 3 
+Ans True 
+Que 4 
+Ans to make single line comment in python use # before the text 
+Que 5 
+Ans false  python is not statically typed lang a statically typed lang means a language in which we have to declare the variable before 
+the compilation """
+
+
+
